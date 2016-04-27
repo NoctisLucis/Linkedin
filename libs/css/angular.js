fc1dcd6383e55ef57f0f40077cf501daf7194b44
NoctisLucis <noctislucispro@gmail.com>
2016-04-27 21:07:31 +0700

    Du lieu dc lay tu file json

---
+++ libs/css/angular.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+angular.module("myapp", []).controller("HeaderController", function($scope) {
+$scope.data = {
+	name : 'Nguyễn Ðình Chung',
+	headline: 'Stundent at University of Science',
+	nation: 'Việt Nam',
+	job: 'Infomation Technology',
+	email: 'noctislucispro@gmail.com',
+	facebook: 'https://www.facebook.com/NoctisLucis.ChungNguyen',
+	};
+});
